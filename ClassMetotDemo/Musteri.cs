f0a3e20e669b479d40d245209c10e3c8a9ee60bd
sun <sun@DESKTOP-GP6QFAV>
2021-03-01 17:47:24 +0000

    Add project files.

---
+++ ClassMetotDemo/Musteri.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class Musteri
+
+    {
+        public int ID { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerSurname { get; set; }
+        public int CustomerAge { get; set; }
+        public string CustomerSex { get; set; }
+
+
+
+
+
+    }
+
+
+}
